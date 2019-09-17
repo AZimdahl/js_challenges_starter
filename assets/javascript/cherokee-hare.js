7e9d78f4ef2cdfe0e3a155d570d55517ff9246f9
Alex Zimdahl <azimdahl91@gmail.com>
2019-09-17 14:35:19 -0400

    Complete cherokee-hare activity

--- assets/javascript/cherokee-hare.js
+++ assets/javascript/cherokee-hare.js
@@ -1,5 +1,29 @@
 (function(){
+let startingPopulation;
+let birthRate;
+let numberOfWeeks;
+let breedButton = document.querySelector("button");
+let result = document.querySelector("#result");
 
-   
+breedButton.addEventListener("click", breed);
 
+function breed() {
+    getParameters();
+
+    for (let weeks = 1; weeks <= numberOfWeeks; weeks++){
+        startingPopulation = startingPopulation + (startingPopulation*birthRate);
+    }
+    startingPopulation = parseInt(startingPopulation);
+    result.innerHTML = "There will be " + startingPopulation + " Cherokee Hares after " + numberOfWeeks + " weeks"
+}
+
+function getParameters() {
+    startingPopulation = document.querySelector("#startingPopulation").value;
+    birthRate = document.querySelector("#birthRate").value;
+    numberOfWeeks = document.querySelector("#numberOfWeeks").value;
+
+    startingPopulation = parseInt(startingPopulation);
+    birthRate = parseFloat(birthRate)/10;
+    numberOfWeeks = parseInt(numberOfWeeks);
+}
 })();
