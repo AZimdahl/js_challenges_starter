5c44b29d5336b05a4ece144c7345a52172247597
Alex Zimdahl <azimdahl91@gmail.com>
2019-09-17 16:04:16 -0400

    Complete change-machine activity

--- assets/javascript/change-machine.js
+++ assets/javascript/change-machine.js
@@ -1,4 +1,34 @@
 (function(){
+    let amount = 103;
+    let twenties = 0;
+    let tens = 0;
+    let fives = 0;
+    let ones = 0;
 
+    while (amount != 0)
+    {
+        if (amount%20 == 0) {
+            twenties++
+            amount -= 20;
+        }
+        else if (amount%10 == 0)
+        {
+            tens++
+            amount -= 10;
+        }
+        else if (amount%5 == 0)
+        {
+            fives++
+            amount -= 5;
+        }
+        else
+        {
+            ones++
+            amount--;
+        }
+    }
+
+    let change = [twenties + " twenties", tens + " tens", fives + " fives", ones + " ones"];   
     
+    console.log(amount, change);
 })();
