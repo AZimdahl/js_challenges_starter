454e75c3765e920b844d1a317a7a0df9bd6adafd
Alex Zimdahl <azimdahl91@gmail.com>
2019-09-19 10:13:20 -0400

    Complete finding-pallindromes activity

--- assets/javascript/finding-pallindromes.js
+++ assets/javascript/finding-pallindromes.js
@@ -1,5 +1,24 @@
 (function(){
-    let word;
-    let reverseWord;
+    let runButton = document.querySelector("button");
+
+    runButton.addEventListener("click", wordCheck);
+
+    function wordCheck() {
+        let word = document.querySelector("#word").value;
+        
+        //source: https://medium.com/sonyamoisset/reverse-a-string-in-javascript-a18027b8e91c
+        let splitString = word.split("");
+        let reverseArray = splitString.reverse();
+        let reverseWord = reverseArray.join("");
+        if (word == reverseWord){
+            console.log(true);
+            alert("Congratulations! You found a pallindrome! Try another word.")
+        }
+        else {
+            console.log(false)
+            alert("Sorry, that word is not a pallindrome. Try another word.")
+        }
+    }
+
 
 })();
